c3a0fb9857284a969484751ef5c02810a982aae5
Kritika.m <kritika.m@tataelxsi.co.in>
2020-12-19 18:54:29 +0530

    IntialCommit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+k
+;app/src/main/java/com/example/myapplication/MainActivity.kt,0\0\00ed33bb9fa3f2577abe71150520b83c93b6af18
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+{
+Kapp/src/main/java/com/example/myapplication/MovieDetails/DetailsActivity.kt,c\2\c2fcb3df6618449504d975f08e755c3a7fca60e9
---
+++ app/src/main/java/com/example/myapplication/MovieDetails/DetailsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.MovieDetails
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
